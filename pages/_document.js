a9f435edd98ff7e093faeef410b686190b19c104
lanaya66 <38175265+lanaya66@users.noreply.github.com>
2025-07-16 21:36:07 +0800

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* 中文注释: 网站 favicon 设置 */}
+        {/* 中文注释: 网站 favicon 设置 - 支持多种尺寸和格式 */}
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/favicon.ico" />
         
         {/* 中文注释: 页面元数据 */}
         <meta name="theme-color" content="#323335" />
