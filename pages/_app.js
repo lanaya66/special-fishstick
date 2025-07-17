52dc207d9cd69c3c8c71125a3ca62b50b29d1701
lanaya66 <38175265+lanaya66@users.noreply.github.com>
2025-07-17 12:39:30 +0800

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -3,5 +3,10 @@ import '../src/i18n/i18n'
 
 //-------------- 全局应用组件 --------------
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="relative min-h-screen">
+      {/* 中文注释: 主要页面内容 */}
+      <Component {...pageProps} />
+    </div>
+  )
 } 
